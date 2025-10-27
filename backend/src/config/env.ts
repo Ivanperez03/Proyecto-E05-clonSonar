ce478607e4849726ca32962af5ef99a655e701ba
Ivanperez03 <alu0101477510@ull.edu.es>
2025-10-27 20:17:13 +0000

    gestion variables entorno

---
+++ backend/src/config/env.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const ENV = {
+  PORT: process.env.PORT ?? '3000',
+  DATABASE_URL: process.env.DATABASE_URL ?? '',
+  NODE_ENV: process.env.NODE_ENV ?? 'development'
+};
+
+if (!ENV.DATABASE_URL) {
+  console.warn('[WARN] DATABASE_URL no está definida');
+}
